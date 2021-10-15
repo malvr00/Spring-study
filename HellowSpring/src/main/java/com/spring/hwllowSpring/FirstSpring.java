9799abfeefe7ac0d6530a4d7c53543b0e5540dc3
조용일 <yedn1128@naver.com>
2021-10-15 13:38:45 +0900

    import delete

--- HellowSpring/src/main/java/com/spring/hwllowSpring/FirstSpring.java
+++ HellowSpring/src/main/java/com/spring/hwllowSpring/FirstSpring.java
@@ -1,8 +1,6 @@
 package com.spring.hwllowSpring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestController;
 
 @Component
 public class FirstSpring {
--- HellowSpring/src/main/java/com/spring/hwllowSpring/MyController.java
+++ HellowSpring/src/main/java/com/spring/hwllowSpring/MyController.java
@@ -1,16 +1,19 @@
 package com.spring.hwllowSpring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MyController{
 	@Autowired
 	private FirstSpring service;
 	
-	@RequestMapping("/welcome")
+	@GetMapping("/welcome")
 	public String welcome() {
+		log.debug("iijii");
 		return service.retrieveWelcomeMessage();
 	}
 }
