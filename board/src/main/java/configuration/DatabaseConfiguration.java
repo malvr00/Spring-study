a40a4488f4f26f01b6e5133dae525f6601882252
조용일 <yedn1128@naver.com>
2021-11-17 23:30:36 +0900

    db 연결 ( 이슈 해결 중 )

--- board/src/main/java/configuration/DatabaseConfiguration.java
+++ board/src/main/java/configuration/DatabaseConfiguration.java
@@ -1,7 +1,6 @@
 package configuration;
 
 import javax.sql.DataSource;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +13,7 @@ import com.zaxxer.hikari.HikariDataSource;
 @PropertySource("classpath:/application.properties")	// 설정파일 사용할 수 있게해줌. 추가해서 다른것도 사용가능. @PropertySource
 public class DatabaseConfiguration {
 
-	@Bean
+	@Bean	//application.properties에 설정했던 데이터베이스 관련 정보를 사용하도록 지정합니다.
 	@ConfigurationProperties(prefix="spring.datasource.hikari")
 	public HikariConfig hikariConfig() {
 	  // 커넥션 풀 히카리CP 사용하는데 prefix가 spring.datasource.hikari로 설정되어있게 때문에 
@@ -27,7 +26,9 @@ public class DatabaseConfiguration {
 	public DataSource dataSource() throws Exception{
 		// 히카리CP의 설정 파일을 이용해서 데이터베이스와 연결하는 데이터 소스를 생성
 		DataSource dataSource = new HikariDataSource(hikariConfig());
+		System.out.println("=======================================================================");
 		System.out.println(dataSource.toString());
+		System.out.println("=======================================================================");
 		return dataSource;
 	}
 }
