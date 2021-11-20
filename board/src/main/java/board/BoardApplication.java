ea725ccebe6b250e4ca73209407cfec73dfc30bf
조용일 <yedn1128@naver.com>
2021-11-20 17:18:26 +0900

    application.properties 'url' 이슈 해결 방안 1

--- board/src/main/java/board/BoardApplication.java
+++ board/src/main/java/board/BoardApplication.java
@@ -1,9 +1,16 @@
 package board;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+// Description:
+//Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured.
+//Reason: Failed to determine a suitable driver class
+// 이슈 해결 어노베이션 - 빈파일로 되있을때 해결 방안
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class BoardApplication {
 
 	public static void main(String[] args) {
