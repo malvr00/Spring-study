1fdbfed2621b6274bba28c7d95e995e38f7b439a
조용일 <yedn1128@naver.com>
2021-11-20 17:27:35 +0900

    `url` 이슈 해결
    src/main/java/board 파일 밑에 패킷을 만들어야하는데 src/main/java 밑에 패킷을 만들어서 경로 설정한 application.properties를 읽어오지 못해서 이슈 발생했었음.

--- board/src/main/java/board/configuration/DatabaseConfiguration.java
+++ board/src/main/java/board/configuration/DatabaseConfiguration.java
@@ -1,6 +1,6 @@
-package configuration;
-
+package board.configuration;
 import javax.sql.DataSource;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
