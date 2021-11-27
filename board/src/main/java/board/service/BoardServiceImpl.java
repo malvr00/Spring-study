6f7e5ea4d52c76fef2af4ceb5505034401262e0c
조용일 <yedn1128@naver.com>
2021-11-27 20:31:52 +0900

    Service 생성 ( Description 참조 )
    일반적으로 두 개의 파일로 구성 Service 인터페이스와 이를 구현한 ServiceImpl 클래스

---
+++ board/src/main/java/board/service/BoardServiceImpl.java
@@ -0,0 +1,19 @@
+package board.service;
+
+import board.dto.BoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service	// 해당 클래스가 스프링 MVC의 서비스임을 나타냄
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired
+	private BoardMapper boardMapper;	// DAO 빈을 선언
+	
+	@Override
+	public List<BoardDto> selectBoardList() throws Exception{
+		return boardMapper.selectBoardList();
+	}
+}
