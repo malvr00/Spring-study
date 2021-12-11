12745fbe4c8a3cfbfd8903f8f33aa51cf39a5b44
조용일 <yedn1128@naver.com>
2021-12-11 18:58:40 +0900

    주석 추가

--- board/src/main/java/board/configuration/WebMvcConfiguration.java
+++ board/src/main/java/board/configuration/WebMvcConfiguration.java
@@ -12,6 +12,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		// 인터셉터를 등록합니다. addPathPatterns() 메소드와 excludePathPatterns() 메서드를 이용하여 요청 주소의 패턴과
 		// 제외할 요청 주소의 패턴을 지정하여 선택적으로 적용할 수 있습니다. 여기서는 모든 요청에 대해서 인터셉터를 적용하기 때문에 특별한 패턴은 지정하지 않았음.
+		// 스프링 빈으로 등록
 		registry.addInterceptor(new LoggerInterceptor());
 	}
 }
